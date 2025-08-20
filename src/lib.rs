3636f150f0d33213537b84d215647c0ad04e3036
MathisWellmann <wellmannmathis@gmail.com>
2025-08-20 21:51:16 +0200

    make feature gate `rand` useful

--- src/lib.rs
+++ src/lib.rs
@@ -77,7 +77,7 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 
-// #[cfg(feature = "rand")]
+#[cfg(feature = "rand")]
 mod rand_compatibility;
 #[cfg(not(feature = "unstable_simd"))]
 mod stable_simd;
